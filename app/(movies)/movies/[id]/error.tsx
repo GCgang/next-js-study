1ca8065a29069cea347025595d5da2ce4b6bfcbd
GCgang <powm97@gmail.com>
2024-07-26 21:05:15 +0900

    Feat: error handling

---
+++ app/(movies)/movies/[id]/error.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+export default function ErrorMovieDetail() {
+  return <h2>Error movie detail</h2>;
+}
--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -3,6 +3,8 @@ import { API_URL } from '../app/(home)/page';
 async function getInfo(id: string) {
   const response = await fetch(`${API_URL}/${id}`);
   const json = await response.json();
+  throw new Error('Erorr handling');
+
   return json;
 }
 
