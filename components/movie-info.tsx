d2c87792d4abda8550f21606ac24c011bf35fd79
GCgang <powm97@gmail.com>
2024-07-26 20:59:25 +0900

    Feat: Parallel requests (Suspense)

---
+++ components/movie-info.tsx
@@ -0,0 +1,12 @@
+import { API_URL } from '../app/(home)/page';
+
+async function getInfo(id: string) {
+  const response = await fetch(`${API_URL}/${id}`);
+  const json = await response.json();
+  return json;
+}
+
+export default async function MovieInfo({ id }: { id: string }) {
+  const movie = await getInfo(id);
+  return <h6>{JSON.stringify(movie)}</h6>;
+}
